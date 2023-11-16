44025f6031d96233af3747aed70e02c18c0d7831
okibe1 <okibe4obinna@gmail.com>
2023-11-16 21:22:21 +0000

    created readme file and ritate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in-place.
+
+    :param matrix: The input matrix to be rotated.
+    """
+    # Transpose the matrix
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+
+# Example usage
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
